ef73be3f6c4eb5cc5264de425dea8f04e01f2cfe
harmeko <pierre.lethiec@epitech.eu>
2016-07-06 23:11:40 +0200

    add markers

--- script.js
+++ script.js
@@ -6,20 +6,49 @@
         ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
             GoogleMapApiProviders.configure({
                 // china: true
+                // enlever les controles + disable le zoom souris
             });
         }]
         )
     .service('weather', function ($http) {
-        this.getCityWeather = function () {
-            $http.get('api.openweathermap.org/data/2.5/weather?lat=48.856614&lon=2.3522219000000177$key=0559792372f5068cdb80ff2f1c3d1951').then(function (response){
-                console.log(response.data);
+        this.getCitiesWeather = function (successCallback) {
+            $http.get('http://api.openweathermap.org/data/2.5/group?id=6455259,6454414,6447142,6448047,2973783&APPID=0559792372f5068cdb80ff2f1c3d1951').then(function (response){
+                // transformer en degres Celsius
+                var data = response.data.list;
+                successCallback(data);
+                        // position: {lat: element.coord.lat, lng: element.coord.lon},
+                        // title: element.name,
+                        // icon: 'http://openweathermap.org/img/w/'+element.weather[0].icon+'.png'
             });
+                // console.log(data);
+            // return data;
         };
     })
     .controller('mainCtrl', function(weather, $scope) {
         $scope.map = { center: { latitude: 47.0860214, longitude: 2.1641826}, zoom: 6 };
 
-        weather.getCityWeather();
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position){
+                $scope.userLatLng = {lat: position.coords.latitude, lng: position.coords.longitude};
+            });
+        }
+
+        weather.getCitiesWeather(function (data) {
+            
+            var coordsObj = [];
+
+            for (var key in data)
+            {
+                coordsObj.push({
+                    latitude: data[key].coord.lat,
+                    longitude: data[key].coord.lon,
+                    id: data[key].id,
+                })
+            }
+            $scope.citiesData = coordsObj;
+
+        });
+
     })
 
 })();
